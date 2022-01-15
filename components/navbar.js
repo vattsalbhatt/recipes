dcecc95202ef47d1ac89a815ad0af0184780d2eb
vattsalbhatt <vatsbhatt333@gmail.com>
2022-01-15 12:20:09 +0530

    first Commit

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+function navbar() {
+  return `<div id="nav">
+    <div id="img">
+      <img src="./images/Meltdown Skate Shop Logo.png" alt="logo" />
+    </div>
+    
+    <p id="p1"><a href="./index.html"></a><i class="fas fa-search"></i></p>
+    <p id="p2"><a href="./trending.html"></a>Trending</p>
+    <p id="p3"><a href="./random.html"></a> Random</p>
+    <p id="p4"><a href="./login.html"></a>Login</p>
+  </div>`;
+}
+
+export default navbar;
